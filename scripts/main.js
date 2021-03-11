ce8c3ed2346f7319cd7abc02868b1ec7047670e7
Vasu Rajan <vasurajan101@gmail.com>
2021-03-11 20:41:41 +0530

    responsive video added

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    //for scroll to count
+    var a = 0;
+    $(window).scroll(function() {
+    var oTop = $(".count").offset().top - window.innerHeight;
+    if (a === 0 && $(window).scrollTop() > oTop) {
+        $(".count").each(function () {
+            $(this).prop("Counter",0).animate({
+                Counter: $(this).text()
+            }, {
+                duration: 4000,
+                easing: "swing",
+                step: function (now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
+        });
+        $(".process-video video").trigger("play");
+        a = 1;
+    }
+    });
+
+     
+});
