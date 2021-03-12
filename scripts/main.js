57f58c58286003b7ff3d331bd004729fd52e84a9
Aayushman Chatterjee <aayushman.chatterjee@publicissapient.com>
2021-03-12 09:46:07 +0530

    geolocation added

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,23 @@
 $(document).ready(() => {
-  let a = 0;
-  $(window).scroll(() => {
-    const oTop = $('.count').offset().top - window.innerHeight;
+    var a = 0;
+    $(window).scroll(function() {
+    var oTop = $(".count").offset().top - window.innerHeight;
     if (a === 0 && $(window).scrollTop() > oTop) {
-      $('.count').each(() => {
-        $(this).prop('Counter', 0).animate({
-          Counter: $(this).text(),
-        }, {
-          duration: 4000,
-          easing: 'swing',
-          step(now) {
-            $(this).text(Math.ceil(now));
-          },
+        $(".count").each(function () {
+            $(this).prop("Counter",0).animate({
+                Counter: $(this).text()
+            }, {
+                duration: 4000,
+                easing: "swing",
+                step: function (now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
         });
-      });
-      $('.process-video video').trigger('play');
-      a = 1;
+        $(".process-video video").trigger("play");
+        a = 1;
     }
-  });
+    });
+
+     
 });
